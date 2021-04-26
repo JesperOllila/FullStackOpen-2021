39879bfac307202bb898c962f8b436a364ae5645
JesperOllila <jesper.ollila@protonmail.com>
2021-04-26 19:14:46 +0300

    Osa 1 kurssitiedot tehtävä 1.5 ja key asettaminen childiin

--- Osa1/kurssitiedot/src/App.js
+++ Osa1/kurssitiedot/src/App.js
@@ -11,7 +11,7 @@ const Header = (props) => {
 const Content = (props) => {
   
   let Content = props.parts.map(value => (
-    <p>{value["name"] + " " + value["exercises"]}</p>)
+    <p key={value["name"]}>{value["name"] + " " + value["exercises"]}</p>)
     )
 
   return (
@@ -38,29 +38,30 @@ const Total = (props) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts}/>
-      <Total parts={parts}/>
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
