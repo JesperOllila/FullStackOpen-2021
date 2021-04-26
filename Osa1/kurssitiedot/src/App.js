d575ad274ecea17d355cd2a7f25d557429b5505a
JesperOllila <jesper.ollila@protonmail.com>
2021-04-26 16:49:10 +0300

    Kirjoitusvirheen korjaus App funktiossa

--- Osa1/kurssitiedot/src/App.js
+++ Osa1/kurssitiedot/src/App.js
@@ -47,7 +47,7 @@ const App = () => {
 
     return (
       <>
-        <Part part={part1.name} exercises={part2.exercises}/>
+        <Part part={part1.name} exercises={part1.exercises}/>
         <Part part={part2.name} exercises={part2.exercises}/>
         <Part part={part3.name} exercises={part3.exercises}/>
       </>
