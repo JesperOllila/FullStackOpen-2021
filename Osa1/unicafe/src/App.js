95e570299289c8220172c87c4ce34fbf347eed0d
JesperOllila <jesper.ollila@protonmail.com>
2021-04-28 15:49:26 +0300

    Osa1/unicafe ohjelmakoodi tehtävä 1.8

--- Osa1/unicafe/src/App.js
+++ Osa1/unicafe/src/App.js
@@ -7,6 +7,19 @@ const Button = (props) => (
   </button>
 )
 
+const Statistics = ({good, neutral, bad}) => {
+  return (
+    <div>
+        <h1>statistics</h1>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {good + neutral + bad}</p>
+        <p>average {(good - bad)/(good + neutral + bad)}</p>
+        <p>positive {good/(good + neutral + bad)*100}%</p>
+      </div>
+  )
+}
 
 const App = () => {
 
@@ -23,15 +36,7 @@ const App = () => {
         <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
         <Button handleClick={() => setBad(bad + 1)} text="bad" />
       </div>
-      <div>
-        <h1>statistics</h1>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {good + neutral + bad}</p>
-        <p>average {(good - bad)/(good + neutral + bad)}</p>
-        <p>positive {good/(good + neutral + bad)*100}%</p>
-      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
