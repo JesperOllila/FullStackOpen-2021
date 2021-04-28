0a954c077eac56327342291fc3d8ab757678f5dc
JesperOllila <jesper.ollila@protonmail.com>
2021-04-28 15:56:14 +0300

    Osa1/unicafe ohjelmakoodi tehtävä 1.9

--- Osa1/unicafe/src/App.js
+++ Osa1/unicafe/src/App.js
@@ -8,15 +8,24 @@ const Button = (props) => (
 )
 
 const Statistics = ({good, neutral, bad}) => {
+  
+  const all = good + neutral + bad
+  if (all==0) {
+    return (
+      <div>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
   return (
     <div>
         <h1>statistics</h1>
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
-        <p>all {good + neutral + bad}</p>
-        <p>average {(good - bad)/(good + neutral + bad)}</p>
-        <p>positive {good/(good + neutral + bad)*100}%</p>
+        <p>all {all}</p>
+        <p>average {(good - bad)/(all)}</p>
+        <p>positive {good/(all)*100}%</p>
       </div>
   )
 }
